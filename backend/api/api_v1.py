586f80b1e663b58cf72ca4c786cbef87b7e14a80
Priyanshu Bhagat <priyanshu0152@gmail.com>
2026-05-15 02:28:19 +0530

    Create api_v1.py

---
+++ backend/api/api_v1.py
@@ -0,0 +1,14 @@
+"""
+backend/api/api_v1.py
+
+API v1 router aggregator.
+"""
+
+from fastapi import APIRouter
+from .routes.system import router as system_router
+from .routes.patient import router as patient_router   # Added in 2B
+
+api_router = APIRouter()
+
+api_router.include_router(system_router, prefix="/system", tags=["System"])
+api_router.include_router(patient_router, prefix="/patient", tags=["Patient"])
